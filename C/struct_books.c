a1d086aafde3992506e860f8225da8c7ee91fdef
human-V-oid <shankergaurineo@gmail.com>
2022-12-04 19:26:21 +0530

    added program for book details through array of structs

---
+++ C/struct_books.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct struct_books{
+    /*Structure to store data of books*/
+
+    int isbn;
+    char book_name[40];
+    char author_name[40];
+    char publisher[30];
+    float price;
+};
+
+int bookDetails(int n)
+{
+    int i;
+    struct struct_books books[n];
+    for (i=0; i<=n-1; i++)
+    {
+        printf("Enter the ISBN: ");
+        scanf("%d", &books[i].isbn);
+        printf("Enter the name of book: ");
+        scanf("%s", &books[i].book_name);
+        printf("Enter the author's name: ");
+        scanf("%s", &books[i].author_name);
+        printf("Enter the name of publisher: ");
+        scanf("%s", &books[i].publisher);
+        printf("Enter the price of book");
+        scanf("%f", &books[i].price);
+    }
+    //printf("")
+    //printf("Choose an option:")
+    return 0;
+}
+
+int main(void)
+{
+    int num;
+
+    printf("Enter the number of books to store details of:");
+    scanf("%d", &num);
+    bookDetails(num);
+}
